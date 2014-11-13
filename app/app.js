e5ebc54727d3e29021fabaec4a2516ef5f704385
Kendoros <bernhard.schatz@bsnetworx.at>
2014-11-14 00:51:53 +0100

    TP-1E * implement simple import-export

--- app/app.js
+++ app/app.js
@@ -30,16 +30,14 @@
             }
         ];
         $scope.template = $scope.templates[0];
-        $scope.playerList = [];
-        $scope.maxPlayerId = 0;
-        $scope.playerCount = 0;
-        $scope.selectedTab = 0;
-
-
         $scope.data = {};
+        $scope.data.playerList = [];
+        $scope.data.maxPlayerId = 0;
+        $scope.data.playerCount = 0;
+        $scope.data.selectedTab = 0;
 
 
-        $scope.configuration = {
+        $scope.data.configuration = {
             points: {
                 win: 3,
                 tie: 2,
@@ -52,90 +50,90 @@
                 name: "",
                 points: 0,
                 winPoints: 0,
-                position: $scope.maxPlayerId,
+                position: $scope.data.maxPlayerId,
                 rank: 0,
-                id: $scope.maxPlayerId
+                id: $scope.data.maxPlayerId
             };
-            $scope.maxPlayerId = $scope.maxPlayerId + 1;
-            $scope.playerList.push(player);
-            $scope.playerCount = $scope.playerList.length;
+            $scope.data.maxPlayerId = $scope.data.maxPlayerId + 1;
+            $scope.data.playerList.push(player);
+            $scope.data.playerCount = $scope.data.playerList.length;
 
         };
 
         $scope.removePlayer = function (index) {
             var newList = [];
-            for (var x = 0; x < $scope.playerList.length; x++) {
+            for (var x = 0; x < $scope.data.playerList.length; x++) {
                 if (x !== index) {
-                    newList.push($scope.playerList[x]);
+                    newList.push($scope.data.playerList[x]);
                 }
             }
 
-            $scope.playerList = newList;
-            $scope.playerCount = $scope.playerList.length;
+            $scope.data.playerList = newList;
+            $scope.data.playerCount = $scope.data.playerList.length;
         }
 
-        $scope.usedGroups = [];
+        $scope.data.usedGroups = [];
         $scope.changeTab = function (tabNumber) {
-            $scope.selectedTab = 0;
-            $scope.selectedTab = tabNumber;
-            $scope.template = $scope.templates[$scope.selectedTab];
-            $scope.usedGroups = [];
+            $scope.data.selectedTab = 0;
+            $scope.data.selectedTab = tabNumber;
+            $scope.template = $scope.templates[$scope.data.selectedTab];
+            $scope.data.usedGroups = [];
 
-            for (var x = 0; x < $scope.playerList.length; x++) {
-                var player = $scope.playerList[x];
+            for (var x = 0; x < $scope.data.playerList.length; x++) {
+                var player = $scope.data.playerList[x];
                 if (player !== undefined) {
                     if (player.groupAssignment) {
                         if (player.groupAssignment.length > 0) {
 
-                            if ($filter('filter')($scope.usedGroups, {code: player.groupAssignment}).length === 0) {
-                                $scope.usedGroups.push({code: player.groupAssignment});
+                            if ($filter('filter')($scope.data.usedGroups, {code: player.groupAssignment}).length === 0) {
+                                $scope.data.usedGroups.push({code: player.groupAssignment});
                             }
                         }
                     }
                 }
             }
 
-            $scope.usedGroups = $filter('orderBy')($scope.usedGroups, "code")
+            $scope.data.usedGroups = $filter('orderBy')($scope.data.usedGroups, "code")
             $scope.changeGroupTab(0);
 
         }
-        $scope.groupCount = 0;
-        $scope.groupSize = 4;
-        $scope.groupGenerated = false;
-        $scope.groups = [];
+        $scope.data.groupCount = 0;
+        $scope.data.groupSize = 4;
+        $scope.data.groupGenerated = false;
+        $scope.data.groups = [];
 
-        $scope.displayGroupIndex = 0;
-        $scope.selectedGroup = {};
+        $scope.data.displayGroupIndex = 0;
+        $scope.data.selectedGroup = {};
 
         $scope.changeGroupTab = function (index) {
-            if ($scope.usedGroups) {
-                if ($scope.usedGroups[index]) {
-                    $scope.selectedGroup.groupAssignment = $scope.usedGroups[index].code;
+            if ($scope.data.usedGroups) {
+                if ($scope.data.usedGroups[index]) {
+                    $scope.data.selectedGroup.groupAssignment = $scope.data.usedGroups[index].code;
                 }
             }
         }
 
-        $scope.koPhase = '';
+        $scope.data.koPhase = '';
         $scope.changeKoPhase = function (index) {
             if (index === 0) {
-                $scope.koPhase = 'winner';
+                $scope.data.koPhase = 'winner';
             } else if (index === 1) {
-                $scope.koPhase = 'loser';
+                $scope.data.koPhase = 'loser';
             }
         }
 
 
         $scope.generateMatchups = function () {
-            $scope.groups = [];
-            for (var x = 0; x < $scope.groupNames.length; x++) {
-                var groupName = $scope.groupNames[x];
+            $scope.data.groups = [];
+            for (var x = 0; x < $scope.data.groupNames.length; x++) {
+                var groupName = $scope.data.groupNames[x];
                 var group = {
                     groupAssignment: groupName,
                     matchups: []
                 }
                 var playerForGroup = [];
-                for (var p = 0; p < $scope.playerList.length; p++) {
-                    var player = $scope.playerList[p];
+                for (var p = 0; p < $scope.data.playerList.length; p++) {
+                    var player = $scope.data.playerList[p];
                     player.winPoints = 0;
                     if (player !== undefined) {
                         if (player.groupAssignment) {
@@ -168,12 +166,12 @@
                     }
 
                 }
-                $scope.groups.push(group);
+                $scope.data.groups.push(group);
             }
         }
         $scope.matchChange = function (group) {
-            for (var x = 0; x < $scope.playerList.length; x++) {
-                var player = $scope.playerList[x];
+            for (var x = 0; x < $scope.data.playerList.length; x++) {
+                var player = $scope.data.playerList[x];
                 if (group.groupAssignment === player.groupAssignment) {
                     var points = 0;
                     var winPoints = 0;
@@ -181,20 +179,20 @@
                         if (group.matchups[y].playerA === player) {
                             points = points + group.matchups[y].pointsPlayerA;
                             if (group.matchups[y].winPlayerA === 'W') {
-                                winPoints = winPoints + $scope.configuration.points.win;
+                                winPoints = winPoints + $scope.data.configuration.points.win;
                             } else if (group.matchups[y].winPlayerA === 'T') {
-                                winPoints = winPoints + $scope.configuration.points.tie;
+                                winPoints = winPoints + $scope.data.configuration.points.tie;
                             } else if (group.matchups[y].winPlayerA === 'L') {
-                                winPoints = winPoints + $scope.configuration.points.loose;
+                                winPoints = winPoints + $scope.data.configuration.points.loose;
                             }
                         } else if (group.matchups[y].playerB === player) {
                             points = points + group.matchups[y].pointsPlayerB;
                             if (group.matchups[y].winPlayerB === 'W') {
-                                winPoints = winPoints + $scope.configuration.points.win;
+                                winPoints = winPoints + $scope.data.configuration.points.win;
                             } else if (group.matchups[y].winPlayerB === 'T') {
-                                winPoints = winPoints + $scope.configuration.points.tie;
+                                winPoints = winPoints + $scope.data.configuration.points.tie;
                             } else if (group.matchups[y].winPlayerB === 'L') {
-                                winPoints = winPoints + $scope.configuration.points.loose;
+                                winPoints = winPoints + $scope.data.configuration.points.loose;
                             }
                         }
                     }
@@ -230,17 +228,17 @@
                 quater.winPlayerB = 'W';
             }
             if ($index === 0) {
-                $scope.winnerBracket.half[0].playerA = winner;
-                $scope.loserBracket.fiveEight[0].playerA = loser;
+                $scope.data.winnerBracket.half[0].playerA = winner;
+                $scope.data.loserBracket.fiveEight[0].playerA = loser;
             } else if ($index === 1) {
-                $scope.winnerBracket.half[1].playerA = winner;
-                $scope.loserBracket.fiveEight[1].playerA = loser;
+                $scope.data.winnerBracket.half[1].playerA = winner;
+                $scope.data.loserBracket.fiveEight[1].playerA = loser;
             } else if ($index === 2) {
-                $scope.winnerBracket.half[0].playerB = winner;
-                $scope.loserBracket.fiveEight[0].playerB = loser;
+                $scope.data.winnerBracket.half[0].playerB = winner;
+                $scope.data.loserBracket.fiveEight[0].playerB = loser;
             } else if ($index === 3) {
-                $scope.winnerBracket.half[1].playerB = winner;
-                $scope.loserBracket.fiveEight[1].playerB = loser;
+                $scope.data.winnerBracket.half[1].playerB = winner;
+                $scope.data.loserBracket.fiveEight[1].playerB = loser;
             }
         }
 
@@ -253,11 +251,11 @@
                 fiveEight.winPlayerB = 'W';
             }
             if ($index === 0) {
-                $scope.loserBracket.fiveSix[0].playerA = winner;
-                $scope.loserBracket.sevenEight[0].playerA = loser;
+                $scope.data.loserBracket.fiveSix[0].playerA = winner;
+                $scope.data.loserBracket.sevenEight[0].playerA = loser;
             } else if ($index === 1) {
-                $scope.loserBracket.fiveSix[0].playerB = winner;
-                $scope.loserBracket.sevenEight[0].playerB = loser;
+                $scope.data.loserBracket.fiveSix[0].playerB = winner;
+                $scope.data.loserBracket.sevenEight[0].playerB = loser;
             }
         }
 
@@ -270,11 +268,11 @@
                 nineTwelve.winPlayerB = 'W';
             }
             if ($index === 0) {
-                $scope.loserBracket.nineTen[0].playerA = winner;
-                $scope.loserBracket.elevenTwelve[0].playerA = loser;
+                $scope.data.loserBracket.nineTen[0].playerA = winner;
+                $scope.data.loserBracket.elevenTwelve[0].playerA = loser;
             } else if ($index === 1) {
-                $scope.loserBracket.nineTen[0].playerB = winner;
-                $scope.loserBracket.elevenTwelve[0].playerB = loser;
+                $scope.data.loserBracket.nineTen[0].playerB = winner;
+                $scope.data.loserBracket.elevenTwelve[0].playerB = loser;
             }
         }
 
@@ -302,15 +300,15 @@
             }
 
             if ($index === 0) {
-                $scope.winnerBracket.final[0].playerA = winner;
-                $scope.winnerBracket.third[0].playerA = loser;
+                $scope.data.winnerBracket.final[0].playerA = winner;
+                $scope.data.winnerBracket.third[0].playerA = loser;
             } else if ($index === 1) {
-                $scope.winnerBracket.final[0].playerB = winner;
-                $scope.winnerBracket.third[0].playerB = loser;
+                $scope.data.winnerBracket.final[0].playerB = winner;
+                $scope.data.winnerBracket.third[0].playerB = loser;
             }
         }
 
-        $scope.tournamentWinner = undefined;
+        $scope.data.tournamentWinner = undefined;
         $scope.clickWinButtonKoPhaseFinals = function (finals, winner) {
             winner.rank = 1;
             if (finals.playerA === winner) {
@@ -323,7 +321,7 @@
                 finals.playerA.rank = 2;
             }
 
-            $scope.tournamentWinner = winner;
+            $scope.data.tournamentWinner = winner;
         }
         $scope.clickWinButtonKoPhaseThird = function (third, winner) {
             winner.rank = 3;
@@ -348,59 +346,59 @@
             }
             return '';
         }
-        $scope.koPhaseGenerated = false;
-        $scope.winnerBracket = {};
-        $scope.loserBracket = {};
+        $scope.data.koPhaseGenerated = false;
+        $scope.data.winnerBracket = {};
+        $scope.data.loserBracket = {};
         $scope.generateKoPhase = function () {
 
             // generate winner bracket:
-            $scope.winnerBracket = {};
-            $scope.winnerBracket.quater = [];
+            $scope.data.winnerBracket = {};
+            $scope.data.winnerBracket.quater = [];
             for (var q = 0; q < 4; q++) {
-                $scope.winnerBracket.quater.push($scope.createMatchupForKo(q));
+                $scope.data.winnerBracket.quater.push($scope.createMatchupForKo(q));
             }
-            $scope.winnerBracket.half = [];
+            $scope.data.winnerBracket.half = [];
             for (var h = 0; h < 2; h++) {
-                $scope.winnerBracket.half.push($scope.createMatchupForKo(h));
+                $scope.data.winnerBracket.half.push($scope.createMatchupForKo(h));
             }
-            $scope.winnerBracket.final = [];
+            $scope.data.winnerBracket.final = [];
             for (var f = 0; f < 1; f++) {
-                $scope.winnerBracket.final.push($scope.createMatchupForKo(f));
+                $scope.data.winnerBracket.final.push($scope.createMatchupForKo(f));
             }
-            $scope.winnerBracket.third = [];
+            $scope.data.winnerBracket.third = [];
             for (var t = 0; t < 1; t++) {
-                $scope.winnerBracket.third.push($scope.createMatchupForKo(t));
+                $scope.data.winnerBracket.third.push($scope.createMatchupForKo(t));
             }
 
             //loser Bracket
-            $scope.loserBracket = {};
-            $scope.loserBracket.fiveEight = [];
+            $scope.data.loserBracket = {};
+            $scope.data.loserBracket.fiveEight = [];
             for (var x1 = 0; x1 < 2; x1++) {
-                $scope.loserBracket.fiveEight.push($scope.createMatchupForKo(x1));
+                $scope.data.loserBracket.fiveEight.push($scope.createMatchupForKo(x1));
             }
-            $scope.loserBracket.nineTwelve = [];
+            $scope.data.loserBracket.nineTwelve = [];
             for (var x2 = 0; x2 < 2; x2++) {
-                $scope.loserBracket.nineTwelve.push($scope.createMatchupForKo(x2));
+                $scope.data.loserBracket.nineTwelve.push($scope.createMatchupForKo(x2));
             }
-            $scope.loserBracket.sevenEight = [];
+            $scope.data.loserBracket.sevenEight = [];
             for (var x3 = 0; x3 < 1; x3++) {
-                $scope.loserBracket.sevenEight.push($scope.createMatchupForKo(x3));
+                $scope.data.loserBracket.sevenEight.push($scope.createMatchupForKo(x3));
             }
-            $scope.loserBracket.elevenTwelve = [];
+            $scope.data.loserBracket.elevenTwelve = [];
             for (var x4 = 0; x4 < 1; x4++) {
-                $scope.loserBracket.elevenTwelve.push($scope.createMatchupForKo(x4));
+                $scope.data.loserBracket.elevenTwelve.push($scope.createMatchupForKo(x4));
             }
-            $scope.loserBracket.fiveSix = [];
+            $scope.data.loserBracket.fiveSix = [];
             for (var x5 = 0; x5 < 1; x5++) {
-                $scope.loserBracket.fiveSix.push($scope.createMatchupForKo(x5));
+                $scope.data.loserBracket.fiveSix.push($scope.createMatchupForKo(x5));
             }
-            $scope.loserBracket.nineTen = [];
+            $scope.data.loserBracket.nineTen = [];
             for (var x6 = 0; x6 < 1; x6++) {
-                $scope.loserBracket.nineTen.push($scope.createMatchupForKo(x6));
+                $scope.data.loserBracket.nineTen.push($scope.createMatchupForKo(x6));
             }
 
 
-            $scope.koPhaseGenerated = true;
+            $scope.data.koPhaseGenerated = true;
         }
 
         $scope.createMatchupForKo = function (number) {
@@ -417,25 +415,28 @@
         }
 
 
-        $scope.groupNames = ["A", "B", "C", "D", "E", "F", "G",
+        $scope.data.groupNames = ["A", "B", "C", "D", "E", "F", "G",
             "H", "I", "J", "K", "L", "M", "N", "O", "P",
             "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-        $scope.groupsForDropDown = [];
+        $scope.data.groupsForDropDown = [];
 
-
-
-        $scope.exportAll = function(){
-
+        $scope.textForExport = "";
+        $scope.textForImport = "";
+        $scope.exportAll = function () {
+            $scope.textForExport = JSON.stringify($scope.data);
+        }
+        $scope.importAll = function ($event) {
+            $scope.data = JSON.parse($event.currentTarget.nextElementSibling.value);
         }
 
 
         // run on initialization
         var generateGroupForDropDown = function () {
-            for (var x = 0; x < $scope.groupNames.length / 2; x++) {
-                $scope.groupsForDropDown.push({
-                    name: "Gruppe " + $scope.groupNames[x],
-                    code: $scope.groupNames[x]
+            for (var x = 0; x < $scope.data.groupNames.length / 2; x++) {
+                $scope.data.groupsForDropDown.push({
+                    name: "Gruppe " + $scope.data.groupNames[x],
+                    code: $scope.data.groupNames[x]
                 });
             }
         }();
